dce2b254ef9f48456af3ce76d0dcf59ef138c2c6
Nicola-Perera <nicola.perera96@mail.com>
2021-10-18 15:48:43 +0200

    snack1: ad condition to print on screen the higgest value

--- snack1/assets/js/main.js
+++ snack1/assets/js/main.js
@@ -6,10 +6,17 @@
     STEPS: tramite 1 prompt chiedo all'utente di inserire un numero;
             faccio la stessa richiesta una seconda volta;
             salvo i 2 numeri inseriti dall'utente in 2 variabili distinte;
-            creo un if per stampare a schermo il numero maggiore,
-            in caso l'utente abbia scritto 2 numeri uguali li stampo entrambi;
+            creo un if per stampare a schermo il numero maggiore;
 */
 
+// 2 values sent by user
 const numberOne = parseFloat(prompt('scrivi un numero'));
 const numberTwo = parseFloat(prompt('scrivi un secondo numero'));
 
+// check what's the higgest value and print it
+if (numberOne >= numberTwo) {
+    console.log(numberOne);
+}
+else if (numberTwo >= numberOne) {
+    console.log(numberTwo);
+}
