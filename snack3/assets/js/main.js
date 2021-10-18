1f0a4c0ce2316e248335c2e6ba55d6a1a4b1ce45
Nicola-Perera <nicola.perera96@mail.com>
2021-10-18 16:57:07 +0200

    snack3: add steps to complete the software

---
+++ snack3/assets/js/main.js
@@ -0,0 +1,8 @@
+// SNACK3: Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+
+/* STEPS: prompt per chiedere all'utente di inserire un numero;
+            ripeto il prompt per 10 volte;
+            salvo in una variabile ogni numero inserito dall'utente;
+            faccio la somma algebrica fra le 10 variabili;
+            mostro a schermo il risultato della somma;
+*/          
