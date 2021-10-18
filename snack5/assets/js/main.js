af87c4f1abc9b3f1d0e00f82ec57a1b8fe61f0cb
Nicola-Perera <nicola.perera96@mail.com>
2021-10-18 18:05:39 +0200

    snack5: add steps to complete the software

---
+++ snack5/assets/js/main.js
@@ -0,0 +1,12 @@
+/*
+    JSnack 5: Crea un array vuoto.
+    Chiedi per 6 volte all’utente di inserire un numero,
+    se è dispari inseriscilo nell’array.
+*/
+
+/*
+ STEPS: creo un ciclo a 6 giri;
+        ad ogni giro chiedo all'utente di inserire un numero intero;
+        se il numero inserito è dispari allora lo inserisco in un array;
+ */
+            
