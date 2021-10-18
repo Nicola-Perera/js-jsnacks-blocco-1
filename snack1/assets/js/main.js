abb9dd35b0195d975023879f90fa929c040a9fa5
Nicola-Perera <nicola.perera96@mail.com>
2021-10-18 15:34:36 +0200

    add steps to complete snack1

---
+++ snack1/assets/js/main.js
@@ -0,0 +1,11 @@
+// JSnack 1
+
+// L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
+
+/*
+    STEPS: tramite 1 prompt chiedo all'utente di inserire un numero;
+            faccio la stessa richiesta una seconda volta;
+            salvo i 2 numeri inseriti dall'utente in 2 variabili distinte;
+            creo un if per stampare a schermo il numero maggiore,
+            in caso l'utente abbia scritto 2 numeri uguali li stampo entrambi;
+*/
