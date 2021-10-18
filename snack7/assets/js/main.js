5660a0b20154b541081df0cb7280a6d0902dece0
Nicola-Perera <nicola.perera96@mail.com>
2021-10-18 18:54:56 +0200

    snack7: add steps to complete the software

---
+++ snack7/assets/js/main.js
@@ -0,0 +1,9 @@
+// JSnack 7: Stampa le potenze di 2 fino a 1000.
+
+/*
+ STEPS: creo un loop con indice che parte da 0;
+        ad ogni giro elevo il 2 a quell'indicie;
+        eseguo il loop fin tanto che il risultato è minore o uguale di 1000;
+        stampo tutti i risultati a schermo;
+*/            
+
