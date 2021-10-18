2cb8eb1970bd3def0e6a7328c6f996fe9a4d8dcd
Nicola-Perera <nicola.perera96@mail.com>
2021-10-18 19:10:07 +0200

    snack8: add steps to complete the software;

---
+++ snack8/assets/js/main.js
@@ -0,0 +1,8 @@
+// JSnack 8: Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
+
+/*
+ STEPS: chiedo all'utente di inserire un numero di 4 cifre;
+        salvo ogni cifra all'interno di una variabile;
+        faccio la somma delle 4 variabili ottenute;
+*/   
+
