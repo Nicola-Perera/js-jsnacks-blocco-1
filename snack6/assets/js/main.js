e4ffe8e64e8daa7fa177dfa35e3d63bec6046440
Nicola-Perera <nicola.perera96@mail.com>
2021-10-18 18:49:51 +0200

    snack6: add loop to calculate the cube of the first N numbers and show the resukts on screen

--- snack6/assets/js/main.js
+++ snack6/assets/js/main.js
@@ -5,3 +5,14 @@
             calcolo il cubo dei primi N numeri;
             mostro a schermo ogni risultato;
 */
+
+// ref to the cubes list in html
+const cubesList = document.getElementById('cubes');
+
+const numberN = parseInt(prompt('inserisci un numero naturale'));
+
+for (i = 0; i < numberN; i++) {
+    const numberCube = i ** 3;
+    cubesList.innerHTML += `<h1 class="cube">il cubo di ${i} è ${numberCube}</h1>`;
+}
+
