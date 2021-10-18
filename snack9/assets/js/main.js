532c0307154e65cddc62ce4c63e085b7c98cedd5
Nicola-Perera <nicola.perera96@mail.com>
2021-10-18 19:43:36 +0200

    snack9: add summ calculation

--- snack9/assets/js/main.js
+++ snack9/assets/js/main.js
@@ -1,7 +1,11 @@
 // JSnack 9: Calcola la somma e la media dei primi 10 numeri.
 
 let number = -1;
+let summ = 0;
 for (let i = 0; i < 10; i++) {
     ++number
-    console.log(number);
+    summ += number;
+    console.log(number, summ);
+   
 }
+
