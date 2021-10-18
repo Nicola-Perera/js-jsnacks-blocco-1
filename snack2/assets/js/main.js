36e4fb4cda6b8d8462d1eb096d36af95785e9729
Nicola-Perera <nicola.perera96@mail.com>
2021-10-18 16:09:27 +0200

    snack2: add steps to complete the program

---
+++ snack2/assets/js/main.js
@@ -0,0 +1,10 @@
+// JSnack2: L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
+
+/* STEPS: tramite prompt chiedo all'utente di inserire una parola;
+            tramite secondo prompt chiedo all'utente di inserire un'altra parola;
+            uso un if per stampare la parola più corta seguita da quella più lunga;
+*/          
+
+// 2 words sent by user
+const wordOne = prompt('scrivi una parola');
+const wordTwo = prompt("scrivi un'altra parola");
