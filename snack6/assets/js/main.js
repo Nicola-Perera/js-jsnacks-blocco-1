e94c1c67130e39fe68d2fde3183ea3c4f55e7019
Nicola-Perera <nicola.perera96@mail.com>
2021-10-18 18:29:37 +0200

    snack6: add steps to complete the software

---
+++ snack6/assets/js/main.js
@@ -0,0 +1,7 @@
+// JSnack 6: Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.
+
+/*
+    STEPS: chiedo all'utente di scrivere un numero naturale N;
+            calcolo il cubo dei primi N numeri;
+            mostro a schermo ogni risultato;
+*/
